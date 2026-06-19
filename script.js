fdd1210aef70afb91ec4e60f091ae45a246e021f
ENSANITYel <96373614+ENSANITYel@users.noreply.github.com>
2026-06-20 02:03:12 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,74 @@
+let movies = [
+  {
+    name: "Hotstar-Clone",
+    des:
+      "This is a hotstar clone created by and the same slider technique was used in this div to make smooth transitioning project slider",
+    image: "assets/trial/hotstarclone.png"
+  },
+  {
+    name: "Login-portal",
+    des:
+      "This is a login portal totally based on node js and HTML with hashed password ",
+    image: "assets/trial/loginportal.png"
+  },
+  {
+    name: "Webin",
+    des:
+      "This is a application based program for website data scrapper for passive information gathering on cyber security ",
+    image: "assets/trial/webin.png"
+  }
+];
+
+const carousel = document.querySelector(".carousel");
+let sliders = [];
+
+let slideIndex = 0; // to track current slide index.
+
+const createSlide = () => {
+  if (slideIndex >= movies.length) {
+    slideIndex = 0;
+  }
+
+  // creating DOM element
+  let slide = document.createElement("div");
+  var imgElement = document.createElement("img");
+  let content = document.createElement("div");
+  let h1 = document.createElement("h1");
+  let p = document.createElement("p");
+
+  // attaching all elements
+  imgElement.appendChild(document.createTextNode(""));
+  h1.appendChild(document.createTextNode(movies[slideIndex].name));
+  p.appendChild(document.createTextNode(movies[slideIndex].des));
+  content.appendChild(h1);
+  content.appendChild(p);
+  slide.appendChild(content);
+  slide.appendChild(imgElement);
+  carousel.appendChild(slide);
+
+  // setting up image
+  imgElement.src = movies[slideIndex].image;
+  slideIndex++;
+
+  // setting elements classname
+  slide.className = "slider";
+  content.className = "slide-content";
+  h1.className = "movie-title";
+  p.className = "movie-des";
+
+  sliders.push(slide);
+
+  if (sliders.length) {
+    sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${
+      30 * (sliders.length - 2)
+    }px)`;
+  }
+};
+
+for (let i = 0; i < 3; i++) {
+  createSlide();
+}
+
+setInterval(() => {
+  createSlide();
+}, 3000);
